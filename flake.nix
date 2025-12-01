9895c02a2bf37123914370493acf01b6fe559340
Dane Lipscombe <danelipscombe@gmail.com>
2025-12-01 12:46:59 +1100

    Add flake build

--- flake.nix
+++ flake.nix
@@ -4,28 +4,51 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      dependencies = with pkgs; [
-        nodejs
-      ];
-    in rec {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs;
-          []
-          ++ dependencies;
-        shellHook = ''
-          export PATH=$(pwd)/node_modules/.bin:node_modules/.bin:$PATH
-        '';
-      };
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        dependencies = with pkgs; [
+          nodejs
+        ];
+      in
+      {
+        packages.default = pkgs.buildNpmPackage {
+          pname = "mokuro-reader";
+          version = "1.0.0";
+
+          # Directory containing package.json & next.config.js
+          src = ./.;
+
+          npmDepsHash = "sha256-aHXSjnR8kR1vIPCveAsIQdWRLdglAZrAqtXpK+BJRWA=";
+          # after first build, replace with the real hash from error message
+
+          buildPhase = ''
+            npm run build
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            mv package.json $out
+            mv node_modules $out
+            mv build $out
+          '';
+        };
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ ] ++ dependencies;
+          shellHook = ''
+            export PATH=$(pwd)/node_modules/.bin:node_modules/.bin:$PATH
+          '';
+        };
+      }
+    );
 }
